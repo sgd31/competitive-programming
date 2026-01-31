c1eb364f55fc3e34f25f6bb7b75ca066c4322f07
sgd31 <gajbhiyetwinkle6@gmail.com>
2026-01-31 15:31:52 +0530

    Update Introductory Problems.cpp

--- CSES/Introductory Problems.cpp
+++ CSES/Introductory Problems.cpp
@@ -1,1 +1,33 @@
+// 1. Weird Algorithm
+#include <iostream>
+using namespace std;
+int main() {
+	long long n;
+	cin >> n;
+	cout << n;
+	while(n > 1){
+	if(n % 2 == 0)
+	    n = n / 2;
+	else
+	    n = (3 * n) + 1;
+	cout << " " << n;
+}
+
+// 2. Missing Number
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+	ll n;
+	cin >> n;
+	ll t, s = 0;
+	t = n * (n + 1) / 2;
+	for(int i = 0; i < n - 1; i++){
+	    ll m;
+	    cin >> m;
+	    s += m;
+	}
+	cout << t - s << endl;
+}
 
