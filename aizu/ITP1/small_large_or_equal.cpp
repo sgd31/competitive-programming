1b72c033bf1b375902bbba2b8f003799f00e53de
sgd31 <gajbhiyetwinkle6@gmail.com>
2026-02-01 14:55:07 +0530

    Create small_large_or_equal.cpp

---
+++ aizu/ITP1/small_large_or_equal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if(a > b)
+    cout << "a > b" << "\n";
+    else if(b > a)
+    cout << "a < b" << "\n";
+    else
+    cout << "a == b" << "\n";
+}
