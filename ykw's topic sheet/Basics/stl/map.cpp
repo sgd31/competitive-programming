0381d5ab83a1c65cddb76f24b53500ce4c228282
sgd31 <gajbhiyetwinkle6@gmail.com>
2026-02-05 16:56:35 +0530

    Update map.cpp

--- ykw's topic sheet/Basics/stl/map.cpp
+++ ykw's topic sheet/Basics/stl/map.cpp
@@ -1,4 +1,5 @@
 /* template, gen info
+
 std::map <key_type, data_type>
 
 map<string,int>m; -> declaration
@@ -13,7 +14,7 @@ map<string,int>::iterator itr=m.find(val); -> find val
 
 itr==m.end() -> find if not key val*/
 
-//1. MAPS 
+// 1. MAPS 
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -43,3 +44,52 @@ int main() {
     } 
     return 0;
 }
+
+// 2. FIGUREFUL
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	map<pair<int, int>, string> mp;
+	while(n--) {
+		int a, b;
+		string s;
+		cin >> a >> b >> s;
+		mp[{a, b}] = s;
+	}
+	int t;
+	cin >> t;
+	while(t--) {
+		int x, y;
+		cin >> x >> y;
+		if(mp.find({x, y}) != mp.end())
+		cout << mp[{x, y}] << "\n";
+	}
+	return 0;
+}
+
+// 3. Hay Points
+
+	int m, n;
+	cin >> m >> n;
+	map<string,int> mp;
+	for(int i = 0; i < m; i++) {
+		string s;
+		int a;
+		cin >> s >> a;
+		mp[s] += a;
+	}
+	while(n--) {
+		long long sum = 0;
+		string s;
+		while(cin >> s) {
+			if(s == ".")
+			break;
+			sum += mp[s];
+		}
+		cout << sum << "\n";
+	}
