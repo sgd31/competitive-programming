e910abea61ccfef3ea437728cdf6ac82ee8eb5c8
sgd31 <gajbhiyetwinkle6@gmail.com>
2026-02-06 08:24:42 +0530

    Update problem_for_beginners.cpp

--- Timus/problem_for_beginners.cpp
+++ Timus/problem_for_beginners.cpp
@@ -1,6 +1,17 @@
-// a + b problem
+//1. a + b problem
 
 int a, b;
 cin >> a >> b;
 cout << a + b << "\n";
 
+//2. reverse root
+
+vector<long long> a;
+	long long x;
+	while(cin >> x){
+		a.push_back(x);
+	}
+	for(int i = a.size() - 1; i >= 0; i--){
+		double res = sqrt(a[i]);
+		cout << fixed << setprecision(4) << res << "\n";
+	}
