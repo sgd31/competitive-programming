2bf31691ecdae44ee4c0bdbccc1b0bd6915a259e
sgd31 <gajbhiyetwinkle6@gmail.com>
2026-02-06 08:21:09 +0530

    Update introductory_problems.cpp

--- CSES/introductory_problems.cpp
+++ CSES/introductory_problems.cpp
@@ -1,4 +1,4 @@
-//
+// missing number
 
 #include <iostream>
 using namespace std;
@@ -14,4 +14,18 @@ int main() {
 	    s += m;
 	}
 	cout << t - s << "\n";
+}
+
+//weird algorithm
+
+	long long n;
+	cin >> n;
+	cout << n;
+while(n > 1) {
+	if(n % 2 == 0)
+	    n = n / 2;
+	else
+	    n = (3 * n) + 1;
+	cout << " " << n;
+	}
 }
