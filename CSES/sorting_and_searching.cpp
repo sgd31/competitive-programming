50ca1de5c5a9f2d272380dc2732d360013c63b51
sgd31 <gajbhiyetwinkle6@gmail.com>
2026-02-07 09:16:48 +0530

    Create sorting_and_searching.cpp

---
+++ CSES/sorting_and_searching.cpp
@@ -0,0 +1,21 @@
+//1. distinct numbers
+
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	set<int> s;
+	vector<int> v(n);
+	for(int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		s.insert(x);
+	}
+	cout << s.size();
+    return 0;
+}
