affc06c56dfdc4ec23daf9150966a3874a9b91db
sgd31 <gajbhiyetwinkle6@gmail.com>
2026-03-31 16:50:01 +0530

    Create ch1-1.cpp

---
+++ cp4/ch1-1.cpp
@@ -0,0 +1,17 @@
+//1. I/O Statements 
+
+//UVa 10071- Back to High School ... 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int v = 0, t = 0, count = 0;
+	while(cin >> v >> t) {
+		cout << 2 * v * t << '\n';
+		count++;
+	}
+	return 0;
+}
+
+//UVa 11614- Etruscan Warriors ...
+
